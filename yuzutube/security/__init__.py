02ce3f7c4ea43b0d0fdc35d4fa157d8e125601b7
ゆず <f.toiro728@gmail.com>
2026-07-07 16:58:23 +0900

    Update __init__.py

--- yuzutube/security/__init__.py
+++ yuzutube/security/__init__.py
@@ -1,6 +1,24 @@
 """セキュリティパッケージ"""
 
-from .crypto import _crypto_engine, _QuantumSignature, _TokenVault, _obfuscated_decorator
+try:
+    from .crypto import _crypto_engine, _QuantumSignature, _TokenVault, _obfuscated_decorator
+except ImportError as e:
+    import warnings
+    warnings.warn(f"Crypto import error (Vercel compatible): {e}")
+    # フォールバック用のダミー実装
+    class _DummyCrypto:
+        def __init__(self):
+            self.tokens = type('obj', (object,), {
+                'issue': lambda *args, **kwargs: "dummy_token"
+            })()
+            self.signature = type('obj', (object,), {
+                'sign': lambda *args: "dummy_signature"
+            })()
+    
+    _crypto_engine = _DummyCrypto()
+    _QuantumSignature = type('_QuantumSignature', (), {})
+    _TokenVault = type('_TokenVault', (), {})
+    _obfuscated_decorator = lambda f: f
 
 __all__ = [
     '_crypto_engine',
